f439cdd79ccceebe4f0ba4c546efd7e3d94c3f98
Romain Desjours <romain@desjours.fr>
2020-08-23 13:19:21 +0200

    Gestion des utilisateurs et liaison avec les annonces

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -49,6 +49,9 @@ class AdController extends AbstractController
                 $manager->persist($image);
 
             }
+
+            $ad->setAuthor($this->getUser());
+
             $manager->persist($ad);
             $manager->flush();
 
