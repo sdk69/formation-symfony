1f47d1a07e3bfcc12a043d887a7fe01e722dd54a
Romain Desjours <romain@desjours.fr>
2020-09-27 16:53:23 +0200

    mise en place de encore pour nos css et js

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,16 +33,13 @@ class HomeController extends AbstractController  {
     /** 
     * @Route("/", name="homepage")
     */
-    public function home(){
-
-        $prenoms = ["Lior" => 31, "Joseph"=>12, "Anne"=>55];
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
 
         return $this->render(
             'home.html.twig',
             [
-                'title' => "Bonjour à tous",
-                'age' => 17,
-                'tableau' => $prenoms
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
             ]
         );
     }
